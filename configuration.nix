d357545da96621d9ca2d3a0efe5f69ca243b8c8f
joshua hammer <jph33@outlook.com>
2025-02-21 17:12:07 -0500

    services.pulseaudio warning rename

--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,7 @@
   system,
   pkgs,
   inputs,
+  config,
   ...
 }: {
   imports = [
@@ -139,24 +140,19 @@
     })
   ];
   # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.ratbagd = {
-    enable = true;
-  };
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  security.rtkit.enable = true;
+  # Enable sound with pipewire.
+  services = {
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+    printing.enable = true;
+    ratbagd.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -206,7 +202,7 @@
       })
     ];
 
-    pkgs = import inputs.unstable {
+    pkgs = import inputs.nixpkgs {
       inherit overlays system;
       config.allowUnfree = true;
     };
