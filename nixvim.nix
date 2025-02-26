484743265ff10c2732a697ed613eb1eab0c4afea
Joshua Hammer <jph33@outlook.com>
2025-02-25 22:50:24 -0500

    nixvim plugin update

--- nixvim.nix
+++ nixvim.nix
@@ -44,6 +44,44 @@
     };
 
     plugins = {
+      telescope = {
+        # seacrhes for for things recursively in current directory
+        enable = true;
+        extensions.fzf-native.enable = true;
+        keymaps = {
+          # NOTE: <leader> = `spacebar`
+          "<leader>pf" = "find_files"; # search for files in directory recursively
+          "<leader>ps" = "live_grep"; # search for a word inside the files in directory recursively
+          "<leader>pr" = "lsp_references"; # when on a variable/method, show everywhere it is used
+        };
+      };
+
+      harpoon = {
+        # lets you quickly save common files and teleport to them
+        enable = true;
+        menu = {
+          width = 100;
+          height = 6;
+        };
+        keymaps = {
+          addFile = "<leader>a"; # save current file to list of files
+          toggleQuickMenu = "<leader>s"; # show list of files
+          navFile = {
+            "1" = "<C-A-j>"; # `control alt j` for first file in list
+            "2" = "<C-A-k>";
+            "3" = "<C-A-l>";
+            "4" = "<C-A-;>";
+          };
+        };
+      };
+
+      todo-comments.enable = true; # TODO: lets you do this, # FIX: and this, # BUG: and this
+
+      # opens the current directory inside a vim window, and lets you add files just by going into insert mode and typing a filename amoung other things
+      oil.enable = true; # NOTE: the open keybind is `-` and is declared down below
+      comment.enable = true; # NOTE: `gcc` to comment a line, or select lines and then `gc`
+      web-devicons.enable = true; # cool icons
+      nvim-autopairs.enable = true; # matches item when typing an (, [, or {
       markdown-preview.enable = true;
       autoclose.enable = true;
       luasnip = {
@@ -125,7 +163,7 @@
           clangd.enable = true;
           cssls.enable = true;
           html.enable = true;
-          java_language_server.enable = true;
+          jdtls.enable = true; # NOTE: this is a much better java lsp
           jsonls.enable = true;
           ltex.enable = true;
           marksman.enable = true;
@@ -146,6 +184,7 @@
       treesitter = {
         enable = true;
         settings = {
+          auto_install = true;
           highlight.enable = true;
         };
       };
@@ -154,20 +193,27 @@
     extraConfigLua = ''
       require("typst-preview").setup()
     '';
+    keymaps = [
+      {
+        mode = ["n"];
+        key = "<leader>pt";
+        action = "<cmd>TodoTelescope<cr>"; # NOTE: opens telescope with all the places you have todo related comments
+      }
+      {
+        mode = ["n"];
+        key = "-";
+        action = "<cmd>Oil<cr>"; # NOTE: opens telescope with all the places you have todo related comments
+      }
+      {
+        mode = ["n"];
+        key = "<Esc>";
+        action = "<cmd>nohlsearch<CR><Esc>"; # NOTE: disables highlight from search, after hitting escape
+      }
+    ];
 
     extraPlugins = with pkgs.vimPlugins; [
-      vim-visual-multi
-      (pkgs.vimUtils.buildVimPlugin
-        {
-          pname = "typst-preview.nvim";
-          version = "1.2.0";
-          src = pkgs.fetchFromGitHub {
-            owner = "chomosuke";
-            repo = "typst-preview.nvim";
-            rev = "ebf10fabde7406848d165b1ef13bce5d046195be";
-            sha256 = "sha256-J3/xxtTk+NyD/98kBoDFy/Zx0fLht0ILRskKZR6HHUI=";
-          };
-        })
+      vim-visual-multi # NOTE: `control n` to do multi cursor when you are on a word
+      typst-preview-nvim # NOTE: they added this to nixpkgs yay!
     ];
   };
 }
